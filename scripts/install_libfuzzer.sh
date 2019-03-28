7c4c3e742887a3dc6b2316dd39d67dc8a6e08706
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-03-28 11:29:57 +0100

    Fix typo in svn command

--- scripts/install_libfuzzer.sh
+++ scripts/install_libfuzzer.sh
@@ -4,7 +4,7 @@ set -e
 TEMPDIR=$(mktemp -d)
 (
     cd $TEMPDIR
-    svn clone https://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/fuzzer libfuzzer
+    svn co https://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/fuzzer libfuzzer
     mkdir -p build-libfuzzer
     cd build-libfuzzer
     $CXX -O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -stdlib=libc++ -std=c++11 -fPIC -fsanitize=address -fsanitize-address-use-after-scope -fno-sanitize=vptr -c ../libfuzzer/*.cpp -I../libfuzzer
