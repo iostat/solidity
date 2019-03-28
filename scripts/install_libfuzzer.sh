690f99c2910ba280b1b28528458ff0ec484eaec2
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-03-28 15:38:34 +0100

    Changed libFuzzer lib search dir

--- scripts/install_libfuzzer.sh
+++ scripts/install_libfuzzer.sh
@@ -6,7 +6,7 @@ TEMPDIR=$(mktemp -d)
     svn co https://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/fuzzer libfuzzer
     mkdir -p build-libfuzzer
     cd build-libfuzzer
-    CXXFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link -stdlib=libc++"
+    CXXFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link -stdlib=libc++ -L/usr/lib/x86_64-linux-gnu"
     $CXX $CXXFLAGS -std=c++11 -fPIC -fno-sanitize=vptr -c ../libfuzzer/*.cpp -I../libfuzzer
     ar r /usr/lib/libFuzzingEngine.a *.o
 )
