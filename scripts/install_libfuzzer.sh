6e57a6262da64b4271b5c520d75534a2061da362
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-03-28 14:09:48 +0100

    Fix libFuzzingEngine install location

--- scripts/install_libfuzzer.sh
+++ scripts/install_libfuzzer.sh
@@ -8,6 +8,6 @@ TEMPDIR=$(mktemp -d)
     mkdir -p build-libfuzzer
     cd build-libfuzzer
     $CXX -O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -stdlib=libc++ -std=c++11 -fPIC -fsanitize=address -fsanitize-address-use-after-scope -fno-sanitize=vptr -c ../libfuzzer/*.cpp -I../libfuzzer
-    ar r /usr/local/lib/libFuzzingEngine.a *.o
+    ar r /usr/lib/libFuzzingEngine.a *.o
 )
 rm -rf $TEMPDIR
