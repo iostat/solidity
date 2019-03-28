d9768cc803ee0e4698a3a1d311df530bc87cdff8
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-03-28 16:05:02 +0100

    Switch link to libstdc++

--- scripts/install_libfuzzer.sh
+++ scripts/install_libfuzzer.sh
@@ -6,7 +6,7 @@ TEMPDIR=$(mktemp -d)
     svn co https://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/fuzzer libfuzzer
     mkdir -p build-libfuzzer
     cd build-libfuzzer
-    CXXFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link -stdlib=libc++ -I/usr/include/c++/v1"
+    CXXFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link -stdlib=libstdc++"
     $CXX $CXXFLAGS -std=c++11 -O2 -fPIC -fno-sanitize=vptr -c ../libfuzzer/*.cpp -I../libfuzzer
     ar r /usr/lib/libFuzzingEngine.a *.o
 )
