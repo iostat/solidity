2f08a97183e48705ec82cd8c5ccb9f5a5fc7e0b5
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-03-28 12:21:19 +0100

    Install LPM stuff in root dir and invoke protoc

--- scripts/install_lpm.sh
+++ scripts/install_lpm.sh
@@ -7,6 +7,6 @@ TEMPDIR=$(mktemp -d)
     git clone --depth 1 https://github.com/google/libprotobuf-mutator.git LPM-SRC
     mkdir -p LPM-BUILD
     cd LPM-BUILD
-    cmake ../LPM-SRC -GNinja -DLIB_PROTO_MUTATOR_DOWNLOAD_PROTOBUF=ON -DLIB_PROTO_MUTATOR_TESTING=OFF -DCMAKE_BUILD_TYPE=Release && ninja install
+    cmake ../LPM-SRC -GNinja -DLIB_PROTO_MUTATOR_DOWNLOAD_PROTOBUF=ON -DLIB_PROTO_MUTATOR_TESTING=OFF -DCMAKE_BUILD_TYPE=Release && DESTDIR=/usr/local/lib/ ninja install
 )
 rm -rf $TEMPDIR
