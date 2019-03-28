a2cd1a9a68c7fcaab0378412f2441799915edcd6
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-03-28 14:40:11 +0100

    Customized compilation flags per project

--- scripts/install_libfuzzer.sh
+++ scripts/install_libfuzzer.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 set -e
-
 TEMPDIR=$(mktemp -d)
 (
     cd $TEMPDIR
     svn co https://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/fuzzer libfuzzer
     mkdir -p build-libfuzzer
     cd build-libfuzzer
-    $CXX -O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -stdlib=libc++ -std=c++11 -fPIC -fsanitize=address -fsanitize-address-use-after-scope -fno-sanitize=vptr -c ../libfuzzer/*.cpp -I../libfuzzer
+    CXXFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link -stdlib=libc++"
+    $CXX $CXXFLAGS -std=c++11 -fPIC -fno-sanitize=vptr -c ../libfuzzer/*.cpp -I../libfuzzer
     ar r /usr/lib/libFuzzingEngine.a *.o
 )
 rm -rf $TEMPDIR
